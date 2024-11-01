b74f24f01bc304d03063d0a0e8b0b4e1513bf2e1
karinakorpusenko <157098151+karinakorpusenko@users.noreply.github.com>
2024-11-01 22:16:57 +0500

    Add files via upload

---
+++ zd1_up/zd1_up/Cat.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace zd1_up
+{
+     class Cat
+    {
+        private string name;
+        private double ves;
+
+        public string Name // свойство, реализуем инкапсуляцию!
+        {
+            // получение значения - просто возврат name
+            get
+            {
+                return name;
+            }
+            // установка значения - используем проверку
+            set
+            {
+                bool OnlyLetters = true;
+                // ключ. слово value - это то, что хотят свойству присвоить
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                if (OnlyLetters)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                }
+            }
+        }
+        public double Ves
+        {
+            get { return ves; }
+
+            set
+            {
+                bool OnlyLetters = true;
+                // ключ. слово value - это то, что хотят свойству присвоить
+                if (value <= 0)
+                {
+                    OnlyLetters = false;
+                    if (value == 0)
+                        ves = 4;
+                    else
+                        ves = 0;
+                    ves -= value;
+                   
+                }
+                if(OnlyLetters)
+                    ves= value;
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное ves!!!");
+                }
+                value = 0;
+            }
+        }
+
+                public Cat(string CatName,double CatVes)
+        {
+            Name = CatName;
+            Ves = CatVes;
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine($"{name},{ves}:MEOOOOOOOOOW!!!");
+        }
+     
+    }
+}
---
+++ zd1_up/zd1_up/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using zd1_up;
+//1
+
+Console.WriteLine( "Введите кличку");
+string kl=Console.ReadLine();
+Console.WriteLine("Введите vosrast");
+double vos = Convert.ToDouble(Console.ReadLine());
+Cat murzik = new Cat(kl,vos);
+Cat barsik = new Cat("Барсег", 0);
+murzik.Meow();
+barsik.Meow();
+barsik.Name = "Барсик";
+barsik.Meow();
+barsik.Name = "1234";
+barsik.Meow();
+barsik.Ves = -42;
+barsik.Meow();
+
